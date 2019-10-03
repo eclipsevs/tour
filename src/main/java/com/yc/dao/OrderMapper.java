4b3153e944b64e8e1abf81257f975db807f2c43d
eclispevs <591755818@qq.com>
2019-10-03 10:25:32 +0800

    更新

--- src/main/java/com/yc/dao/OrderMapper.java
+++ src/main/java/com/yc/dao/OrderMapper.java
@@ -5,6 +5,7 @@ import com.yc.po.Order;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+
 /**
  * @author wk
  * @date 2019/9/23  13:57
@@ -13,30 +14,35 @@ public interface OrderMapper {
 
     /**
      * 根据酒店id和用户id加入购物车
+     *
      * @param id
      * @param uid
      * @return
      */
-    public int add(@Param("id") Integer id,@Param("uid")Integer uid, @Param("oprice")Double oprice);
+    public int add(@Param("id") Integer id, @Param("uid") Integer uid,
+                   @Param("oprice") Double oprice);
 
     /**
      * 查看全部待付款订单
+     *
      * @param ostatus 状态码
-     * @param uid  用户id
+     * @param uid     用户id
      * @return
      */
     public List<Order> findAll(@Param("ostatus") Integer ostatus, @Param("uid") Integer uid);
 
     /**
      * 修改支付完成的订单状态
-     * @param list 包含要修改的订单号和用户编号
+     *
+     * @param list    包含要修改的订单号和用户编号
      * @param ostatus 状态码
      * @return
      */
-    public int updateStatus(@Param("list") List<Order> list,@Param("ostatus") Integer ostatus);
+    public int updateStatus(@Param("list") List<Order> list, @Param("ostatus") Integer ostatus);
 
     /**
      * 根据id删除订单
+     *
      * @param oid 订单号
      * @return
      */
@@ -46,19 +52,20 @@ public interface OrderMapper {
      * 根据商品号，用户编号和状态号查询购物车中是否有未支付的该商品号的商品，
      * 来判断是插入新数据还是更新数据的数量
      *
-     * @param ocid 商品号
-     * @param uid 用户编号
+     * @param ocid    商品号
+     * @param uid     用户编号
      * @param ostatus 状态号
      * @return
      */
-    Hotel findByIdAndUid(@Param("ocid") Integer ocid, @Param("uid") Integer uid, @Param("ostatus") Integer ostatus );
+    Hotel findByIdAndUid(@Param("ocid") Integer ocid, @Param("uid") Integer uid,
+                         @Param("ostatus") Integer ostatus);
 
     /**
      * 更新订单的商品数量
      *
-     * @param id 订单号
+     * @param id  订单号
      * @param num 修改数量
      * @return
      */
-    int updateNum(@Param("id")Integer id, @Param("num")Integer num);
+    int updateNum(@Param("id") Integer id, @Param("num") Integer num);
 }
